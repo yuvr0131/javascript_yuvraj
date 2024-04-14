ebc8ac50474fb6e23ae8367be29b8cc887475b91
yuvr0131 <yuvr0131@gmail.com>
2024-04-14 20:51:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var age=23;
+//console.log("age is"+ age);
+//console.log(typeof age);
+// comments
+var x="234";
+const y= Number(x);
+//console.log(typeof y);
+let firstname,lastname;
+firstname="yuvraj";
+lastname="singh";
+//console.log(firstname,lastname);
+let a=23;//number
+let b="23";//string
+console.log(a==b);//true
+//strict checking
+console.log(a===b);//false
